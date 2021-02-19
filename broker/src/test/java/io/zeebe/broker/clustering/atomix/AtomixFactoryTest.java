26c817528c35deb0f500b852c625715436c5d985
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-02-19 16:35:53 +0100

    chore(atomix): remove unused configuration
    - removes retainStaleSnapshots and StorageLevel configuration from the
      RaftLog, RaftStorage, and MetaStore, and any references/usages thereof

--- broker/src/test/java/io/zeebe/broker/clustering/atomix/AtomixFactoryTest.java
+++ broker/src/test/java/io/zeebe/broker/clustering/atomix/AtomixFactoryTest.java
@@ -12,7 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.atomix.core.Atomix;
 import io.atomix.raft.partition.RaftPartitionGroup;
 import io.atomix.raft.partition.RaftPartitionGroupConfig;
-import io.atomix.storage.StorageLevel;
 import io.zeebe.broker.system.configuration.BrokerCfg;
 import io.zeebe.snapshots.broker.impl.FileBasedSnapshotStoreFactory;
 import io.zeebe.util.Environment;
@@ -28,36 +27,6 @@ public final class AtomixFactoryTest {
   @Before
   public void setUp() {
     environment = new Environment();
-  }
-
-  @Test
-  public void shouldUseMappedStorageLevel() {
-    // given
-    final var brokerConfig = newConfig();
-    brokerConfig.getData().setUseMmap(true);
-
-    // when
-    final var atomix =
-        AtomixFactory.fromConfiguration(brokerConfig, new FileBasedSnapshotStoreFactory(null));
-
-    // then
-    final var config = getPartitionGroupConfig(atomix);
-    assertThat(config.getStorageConfig().getLevel()).isEqualTo(StorageLevel.MAPPED);
-  }
-
-  @Test
-  public void shouldUseDiskStorageLevel() {
-    // given
-    final var brokerConfig = newConfig();
-    brokerConfig.getData().setUseMmap(false);
-
-    // when
-    final var atomix =
-        AtomixFactory.fromConfiguration(brokerConfig, new FileBasedSnapshotStoreFactory(null));
-
-    // then
-    final var config = getPartitionGroupConfig(atomix);
-    assertThat(config.getStorageConfig().getLevel()).isEqualTo(StorageLevel.DISK);
   }
 
   @Test
